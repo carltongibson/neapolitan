aaadf1b9e8d3ebf5c021249725f88277ec05b8e9
Carlton Gibson <carlton.gibson@noumenal.es>
2023-04-08 15:25:09 +0200

    Added sphinx.ext.autodoc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@ author = 'Carlton Gibson'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
