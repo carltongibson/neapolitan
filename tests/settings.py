7d331fb6ecb302357a2ce9ea944ac528a65dd6a7
Carlton Gibson <carlton.gibson@noumenal.es>
2023-04-08 18:05:33 +0200

    Test template coverage too.

--- tests/settings.py
+++ tests/settings.py
@@ -28,6 +28,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "debug": True,
         },
     },
 ]
