84fc2a3c4cf5fbdf99eaa6d4148b792471ceb24f
Carlton Gibson <carlton.gibson@noumenal.es>
2023-04-10 11:00:57 +0200

    Added boolean favourite field to test model.

--- tests/models.py
+++ tests/models.py
@@ -5,3 +5,4 @@ class Bookmark(models.Model):
     url = models.URLField(unique=True)
     title = models.CharField(max_length=255)
     note = models.TextField(blank=True)
+    favourite = models.BooleanField(default=False)
