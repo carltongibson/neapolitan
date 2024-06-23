702a7c6555a9a356bce9369d97ffb000966adfac
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2024-06-23 00:03:37 +0000

    Duplicate the test project with a more vanilla project structure

---
+++ more_tests/test_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+
+
+class Bookmark(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=255)
+    note = models.TextField(blank=True)
+    favourite = models.BooleanField(default=False)
+
+
+class NamedCollection(models.Model):
+    """For testing UUID lookup"""
+    name = models.CharField(max_length=25, unique=True)
+    code = models.UUIDField(unique=True, default=uuid.uuid4)
